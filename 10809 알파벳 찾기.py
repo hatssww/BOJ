169ba65e7b578711ce5c168931f12a44db453a8b
hatssww <83650060+hatssww@users.noreply.github.com>
2021-05-25 17:27:37 +0900

    Create 10809 알파벳 찾기.py

---
+++ 10809 알파벳 찾기.py
@@ -0,0 +1,5 @@
+S = input()
+alpahbet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in alpahbet:
+    print(S.find(i), end=" ")
